bd256ae8627d4a3b9f691374f5c8952b51876378
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-12-15 07:01:43 +0100

    Using supergraph-orm

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,14 @@
-import {
-  GraphQLResolveInfo,
-  ExecutionResult,
-  GraphQLSchema,
-  InlineFragmentNode,
-} from 'graphql'
-import { sign } from 'jsonwebtoken'
-import { request, GraphQLClient } from 'graphql-request'
-import { GraphcoolLink } from './GraphcoolLink'
-import { importSchema } from 'graphql-import'
-import { SchemaCache } from 'graphql-schema-cache'
-import { delegateToSchema, makeRemoteExecutableSchema } from 'graphql-tools'
-import {
-  buildTypeLevelInfo,
-  buildExistsInfo,
-  buildFragmentInfo,
-} from './prepareInfo'
-import { FragmentReplacements } from './extractFragmentReplacements'
+import { GraphQLSchema } from 'graphql';
+import { importSchema } from 'graphql-import';
+import { SchemaCache } from 'graphql-schema-cache';
+import { delegateToSchema } from 'graphql-tools';
+import { sign } from 'jsonwebtoken';
+import { FragmentReplacements, Orm } from 'supergraph-orm';
 
-export { extractFragmentReplacements } from './extractFragmentReplacements'
+import { GraphcoolLink } from './GraphcoolLink';
+import { buildExistsInfo } from './prepareInfo';
+
 export { DateTimeResolver as DateTime } from './DateTimeResolver'
-
-export interface Query {
-  [rootField: string]: <T = any>(
-    args?: any,
-    info?: GraphQLResolveInfo | string,
-  ) => Promise<T>
-}
 
 export interface Exists {
   [rootField: string]: (filter: { [key: string]: any }) => Promise<boolean>
@@ -42,14 +25,8 @@ const typeDefsCache: { [schemaPath: string]: string } = {}
 const schemaCache = new SchemaCache()
 
 // TODO extra case: use `ctx.db.Place({ id }, query)` but delegate to a sub type (aggregate)
-export class Graphcool {
-  query: Query
-  mutation: Query
+export class Graphcool extends Orm {
   exists: Exists
-
-  private remoteSchema: GraphQLSchema
-  private fragementReplacements: FragmentReplacements
-  private graphqlClient: GraphQLClient
 
   constructor({
     schemaPath,
@@ -57,8 +34,6 @@ export class Graphcool {
     secret,
     fragmentReplacements,
   }: GraphcoolOptions) {
-    fragmentReplacements = fragmentReplacements || {}
-
     const typeDefs = getCachedTypeDefs(schemaPath)
     const token = sign({}, secret)
     const link = new GraphcoolLink(endpoint, token)
@@ -69,113 +44,9 @@ export class Graphcool {
       key: endpoint,
     })
 
-    this.query = new Proxy(
-      {},
-      new QueryHandler(remoteSchema, fragmentReplacements),
-    )
-    this.mutation = new Proxy(
-      {},
-      new MuationHandler(remoteSchema, fragmentReplacements),
-    )
+    super({executableSchema: remoteSchema, fragmentReplacements})
+
     this.exists = new Proxy({}, new ExistsHandler(remoteSchema))
-
-    this.remoteSchema = remoteSchema
-    this.fragementReplacements = fragmentReplacements
-
-    this.graphqlClient = new GraphQLClient(endpoint, {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-  }
-
-  async request<T = any>(
-    query: string,
-    variables?: { [key: string]: any },
-  ): Promise<T> {
-    return this.graphqlClient.request<T>(query, variables)
-  }
-
-  async delegate(
-    operation: 'query' | 'mutation' | 'subscription',
-    fieldName: string,
-    args: {
-      [key: string]: any
-    },
-    context: {
-      [key: string]: any
-    },
-    info: GraphQLResolveInfo,
-  ) {
-    return delegateToSchema(
-      this.remoteSchema,
-      this.fragementReplacements,
-      operation,
-      fieldName,
-      args,
-      context,
-      info,
-    )
-  }
-}
-
-class QueryHandler implements ProxyHandler<Graphcool> {
-  constructor(
-    private schema: GraphQLSchema,
-    private fragmentReplacements: FragmentReplacements,
-  ) {}
-
-  get(target, prop: string) {
-    return (
-      args?: { [key: string]: any },
-      info?: GraphQLResolveInfo,
-    ): Promise<ExecutionResult> => {
-      const operation = 'query'
-      if (!info) {
-        info = buildTypeLevelInfo(prop, this.schema, operation)
-      } else if (typeof info === 'string') {
-        info = buildFragmentInfo(prop, this.schema, operation, info)
-      }
-
-      return delegateToSchema(
-        this.schema,
-        this.fragmentReplacements,
-        operation,
-        prop,
-        args || {},
-        {},
-        info,
-      )
-    }
-  }
-}
-
-class MuationHandler implements ProxyHandler<Graphcool> {
-  constructor(
-    private schema: GraphQLSchema,
-    private fragmentReplacements: FragmentReplacements,
-  ) {}
-
-  get(target, prop: string) {
-    return (
-      args: { [key: string]: any },
-      info?: GraphQLResolveInfo | string,
-    ): Promise<ExecutionResult> => {
-      const operation = 'mutation'
-      if (!info) {
-        info = buildTypeLevelInfo(prop, this.schema, operation)
-      } else if (typeof info === 'string') {
-        info = buildFragmentInfo(prop, this.schema, operation, info)
-      }
-
-      return delegateToSchema(
-        this.schema,
-        this.fragmentReplacements,
-        operation,
-        prop,
-        args,
-        {},
-        info,
-      )
-    }
   }
 }
 
