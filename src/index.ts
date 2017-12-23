73724a7de4af2a9e52df4f8334894c5fbf68a8b2
Johannes Schickling <schickling.j@gmail.com>
2017-12-23 17:13:33 +0100

    reexpose extract fragments

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Graphcool } from './Graphcool'
 export { makeGraphcoolLink } from './link'
 export { BaseGraphcoolOptions } from './types'
+export { extractFragmentReplacements } from 'graphql-binding'
