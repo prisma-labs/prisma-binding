8593ce60db12ba871de6dcb9895a682e96f71570
Johannes Schickling <schickling.j@gmail.com>
2017-12-30 13:17:58 +0100

    feat: Allow env vars instead of endpoint & secret constructor option
    Instead of providing the `endpoint` and `secret` constructor option, you can now set the
    `GRAPHCOOL_ENDPOINT` and `GRAPHCOOL_SECRET` env var.

--- src/Graphcool.ts
+++ src/Graphcool.ts
@@ -15,32 +15,51 @@ export class Graphcool extends Binding {
   exists: Exists
 
   constructor({
-    schemaPath,
     typeDefs,
     endpoint,
     secret,
     fragmentReplacements,
     debug,
   }: GraphcoolOptions) {
-    if (!schemaPath && !typeDefs) {
-      throw new Error('Provide either schemaPath or typeDefs')
+    if (typeDefs.endsWith('.graphql')) {
+      typeDefs = getCachedTypeDefs(typeDefs)
     }
 
-    if (schemaPath) {
-      typeDefs = getCachedTypeDefs(schemaPath)
+    if (endpoint === undefined) {
+      if (process.env.GRAPHCOOL_ENDPOINT) {
+        endpoint = process.env.GRAPHCOOL_ENDPOINT
+      } else {
+        throw new Error(
+          `No Graphcool endpoint found. Either provide \`endpoint\` constructor option or set \`GRAPHCOOL_ENDPOINT\` env var.`,
+        )
+      }
+    }
+
+    if (!endpoint!.startsWith('http')) {
+      throw new Error(`Invalid Graphcool endpoint provided: ${endpoint}`)
+    }
+
+    if (secret === undefined) {
+      if (process.env.GRAPHCOOL_SECRET) {
+        secret = process.env.GRAPHCOOL_SECRET
+      } else {
+        throw new Error(
+          `No Graphcool secret found. Either provide \`secret\` constructor option or set \`GRAPHCOOL_SECRET\` env var.`,
+        )
+      }
     }
 
     fragmentReplacements = fragmentReplacements || {}
 
     debug = debug || false
 
-    const token = sign({}, secret)
-    const link = makeGraphcoolLink({ endpoint, token, debug })
+    const token = sign({}, secret!)
+    const link = makeGraphcoolLink({ endpoint: endpoint!, token, debug })
 
     const remoteSchema = schemaCache.makeExecutableSchema({
       link,
-      typeDefs: typeDefs!,
-      key: endpoint,
+      typeDefs,
+      key: endpoint!,
     })
 
     super({ schema: remoteSchema, fragmentReplacements })
--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,16 @@
-import { FragmentReplacements } from 'graphql-binding';
+import { FragmentReplacements } from 'graphql-binding'
 
 export interface Exists {
   [rootField: string]: (filter: { [key: string]: any }) => Promise<boolean>
 }
 
-export interface GraphcoolOptions extends BaseGraphcoolOptions {
-  schemaPath?: string,
-  typeDefs?: string
-}
-
 export interface BaseGraphcoolOptions {
   fragmentReplacements?: FragmentReplacements
-  endpoint: string
-  secret: string
+  endpoint?: string
+  secret?: string
   debug?: boolean
+}
+
+export interface GraphcoolOptions extends BaseGraphcoolOptions {
+  typeDefs: string
 }
