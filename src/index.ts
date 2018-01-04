f0fad1e1dcbddaf0fd5697aa0c9d88b2497a76fd
Kim Brandwijk <kim.brandwijk@gmail.com>
2018-01-04 14:12:39 +0100

    feat: expose forwardTo function from graphql-binding (#46)
    Closes #45

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Graphcool } from './Graphcool'
 export { makeGraphcoolLink } from './link'
 export { BaseGraphcoolOptions } from './types'
-export { extractFragmentReplacements } from 'graphql-binding'
+export { extractFragmentReplacements, forwardTo } from 'graphql-binding'
