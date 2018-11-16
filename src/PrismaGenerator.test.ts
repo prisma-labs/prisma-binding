b60ede976c6008ecf4b0275180850a816b47cbe6
Matic Zavadlal <matic.zavadlal@gmail.com>
2018-11-16 22:14:27 +0100

    ava fix

--- src/PrismaGenerator.test.ts
+++ src/PrismaGenerator.test.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs'
 import * as path from 'path'
 import { buildSchema } from 'graphql'
 import { PrismaGenerator } from './PrismaGenerator'
-import { test } from 'ava'
+import test from 'ava'
 
 const typeDefs = fs.readFileSync(
   path.join(__dirname, '../src/fixtures/schema.graphql'),
